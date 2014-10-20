d7eba195e4a2c7317dcb763333b709d6041869ad
Sasha Zherebylo <savzhe@gmail.com>
2014-10-20 23:19:52 +0300

    help

--- robohub/api.js
+++ robohub/api.js
@@ -1,3 +1,15 @@
 robohub && (robohub.api = {
-    help: function () {}
+    help: function () {
+        if(robohub.api.help) {
+            var i, helpString = '';
+            for (i in robohub.api.helpText) {
+                helpString += i + ' - ' + robohub.api.helpText[i] + '\n';
+            }
+            kernel.echo(helpString.slice(0, -1));
+            return 'help - for help'
+        }
+    },
+    helpText: {
+        map: 'show map'
+    }
 });
